d56a9f3f027a5e7bb9fdb02e2263a49ca52c67fa
Djomlatoir <nedeljkovicmladen997@gmail.com>
2024-05-21 19:52:34 +0200

    test commit from ubuntu

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
-def factorial(n):
-	if n == 0:
-		return 1
-	else:
-		return n* factorial(n-1)
-number = 5
-result = factorial(number)
-print(f"The factorial of {number} is {result}.")
+from flask import Flask
 
+app = Flask(__name__)
+
+def hello():
+	return 'Hello, world!'
+
+if __name__ == '__main__':
+	app.run(debug=True, host='0.0.0.0')
+	
